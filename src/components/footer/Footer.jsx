a71adbed41e0a8de37235122499066248c8bc6b0
tayuprosper <91825356+tayuprosper@users.noreply.github.com>
2023-11-29 22:02:44 +0100

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='bg-slate-950 p-5 flex flex-col justify-center items-center text-white '>
-      <p className='text-center'>Made <span className=' text-red-700'>&#9829;</span> with<br/>  By LoudLad</p>
+      <p className='text-center'>Made  with <span className=' text-red-700'>&#9829;</span><br/>  By LoudLad</p>
     </div>
   )
 }
