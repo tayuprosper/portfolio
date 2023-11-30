fa4257ea71b8de7b2465e9827e15a067f03fd3ff
tayuprosper <91825356+tayuprosper@users.noreply.github.com>
2023-11-30 08:00:48 +0100

    Update contacts.jsx

--- src/components/contact/contacts.jsx
+++ src/components/contact/contacts.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faAmbulance} from '@fortawesome/free-solid-svg-icons'
 import Link from 'next/link';
+import { Montserrat } from 'next/font/google'
 import { Facebook } from 'fontawesome';
+const monteserat = Montserrat({weight: '600',subsets: ['cyrillic']})
 export default function Contacts() {
     const phoneNumber = '+237673290032'; // Replace with the recipient's phone number
     const message = "Hello, Tayu, i've seen your'e portfolio and i would like to work with you on my next project"; // Replace with the pre-filled message
@@ -14,7 +16,7 @@ export default function Contacts() {
   return (
     <div className='mx-[3rem] lg:mx-[7rem] my-20' id='contact'>
         <div className="line bg-[#4020d2] h-3 rounded-lg w-1/12 "></div>
-        <h1 className='text-4xl my-6 font-extrabold'>Contact Me</h1>
+        <h1 className={`${monteserat.className} text-4xl my-6 font-extrabold`}>Contact Me</h1>
         <div className="projects flex  justify-evenly gap-4">
             <div className="rounded-lg h-fit flex flex-col lg:flex-row shadow-xl w-full gap-7 bg-white p-4 lg:justify-between">
                 <div className="x flex flex-col items-center hover:cursor-pointer hover:scale-110">
