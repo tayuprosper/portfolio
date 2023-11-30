3b6259104919ca1cf7e58a440603f6fb78e1dba5
tayuprosper <91825356+tayuprosper@users.noreply.github.com>
2023-11-30 11:55:37 +0100

    Update Projects.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const monteserat = Montserrat({weight: '600',subsets: ['cyrillic']})
 function Projects() {
   return (
-    <div className={`${monteserat} mx-[3rem] lg:mx-[6rem]`} id='projects'>
+    <div className={`${monteserat.className} mx-[3rem] lg:mx-[6rem]`} id='projects'>
         <div className="line bg-[#4020d2] h-3 rounded-lg w-1/12 "></div>
         <h1 className='text-4xl my-6 font-extrabold'>My Projects</h1>
         <div className="flex-col lg:flex-row projects flex lg:justify-evenly gap-4  items-center">
